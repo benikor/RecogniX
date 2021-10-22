20e7adbffbe875062e14fdc847dc8a248855a451
kormosbenedek <kormos.benedek@gmail.com>
2021-10-22 22:46:14 +0200

    Add routing to Angular

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DoctorComponent } from './doctor/doctor.component';
+import { LoginComponent } from './login/login.component';
+import { PatientComponent } from './patient/patient.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'patient', component: PatientComponent },
+  { path: 'doctor', component: DoctorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -7,21 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  patient=false
-  doctor=false
-
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  toPatient() {
-    this.patient=true
-    this.doctor=false
-  }
-
-  toDoctor() {
-    this.patient=false
-    this.doctor=true
-  }
 }
