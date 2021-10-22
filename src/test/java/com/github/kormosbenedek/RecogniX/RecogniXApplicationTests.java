61a33f83bdf693ac11a5518bbc8a1c65dd0aea6e
kormosbenedek <kormos.benedek@gmail.com>
2021-10-22 14:41:53 +0200

    Add demo spring boot project

---
+++ src/test/java/com/github/kormosbenedek/RecogniX/RecogniXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kormosbenedek.RecogniX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecogniXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
