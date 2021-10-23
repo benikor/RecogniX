8743438deadf1491a557cd72b50658ace8d1cc67
Kormos-git <kornorbi65@gmail.com>
2021-10-23 13:59:06 +0200

    dummy symptom data

--- backend/src/main/java/com/github/kormosbenedek/RecogniX/dataloader/AllDataLoader.java
+++ backend/src/main/java/com/github/kormosbenedek/RecogniX/dataloader/AllDataLoader.java
@@ -1,7 +1,9 @@
 package com.github.kormosbenedek.RecogniX.dataloader;
 
 import com.github.kormosbenedek.RecogniX.entity.Patient;
-import com.github.kormosbenedek.RecogniX.repositories.PatientCrudRepository;
+import com.github.kormosbenedek.RecogniX.entity.Symptom;
+import com.github.kormosbenedek.RecogniX.entity.Treatment;
+import com.github.kormosbenedek.RecogniX.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,16 +15,42 @@ import java.util.List;
 public class AllDataLoader implements CommandLineRunner {
 
     @Autowired
-    PatientCrudRepository repository;
+    PatientCrudRepository patientCrudRepository;
+    @Autowired
+    SymptomCrudRepository symptomCrudRepository;
+    @Autowired
+    TreatmentCrudRepository treatmentCrudRepository;
+    @Autowired
+    RequestTreatmentCrudRepository requestTreatmentCrudRepository;
+    @Autowired
+    CompletedTreatmentCrudRepository completedTreatmentCrudRepository;
+    @Autowired
+    AutoTreatmentCrudRepository autoTreatmentCrudRepository;
+    private String loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Magna etiam tempor orci eu lobortis elementum nibh tellus. Quis varius quam quisque id diam vel quam elementum pulvinar. Mauris pellentesque pulvinar pellentesque habitant. Velit sed ullamcorper morbi tincidunt ornare massa eget. Nibh ipsum consequat nisl vel pretium lectus quam id. Accumsan tortor posuere ac ut consequat semper viverra.";
 
     public void run(String... args) {
         loadPatientData();
+        loadSymptomData();
+        loadTreatmentData();
     }
 
     private void loadPatientData(){
         List<Patient> patients = new ArrayList<>();
         patients.add(new Patient(1L,"Teszt Elek",18));
         patients.add(new Patient(2L,"Dummy Jhon",55));
-        repository.saveAll(patients);
+        patientCrudRepository.saveAll(patients);
+    }
+    private void loadSymptomData(){
+        List<Symptom> symptoms = new ArrayList<>();
+        symptoms.add(new Symptom(1L,"Headache",loremIpsum));
+        symptoms.add(new Symptom(2L,"Fatigue",loremIpsum));
+        symptoms.add(new Symptom(3L,"Skin redness",loremIpsum));
+        symptoms.add(new Symptom(4L,"Pain",loremIpsum));
+        symptoms.add(new Symptom(5L,"Loss of apetite",loremIpsum));
+        symptoms.add(new Symptom(6L,"Urinary retention",loremIpsum));
+    }
+    private void loadTreatmentData(){
+        List<Treatment> treatments = new ArrayList<>();
+        treatments.add(new Treatment(1L));
     }
 }
