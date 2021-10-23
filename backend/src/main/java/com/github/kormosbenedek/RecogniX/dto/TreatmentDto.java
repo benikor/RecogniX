c14c4e44df6dcbe2cf32af5da0a4e798e75962e8
klucsik <krisztian.klucsik@pontsystems.eu>
2021-10-23 21:37:40 +0200

    autotreatment dummy work

--- backend/src/main/java/com/github/kormosbenedek/RecogniX/dto/TreatmentDto.java
+++ backend/src/main/java/com/github/kormosbenedek/RecogniX/dto/TreatmentDto.java
@@ -1,12 +1,15 @@
 package com.github.kormosbenedek.RecogniX.dto;
 
 import com.github.kormosbenedek.RecogniX.enums.TreatmentRecommendation;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class TreatmentDto {
     private Long id;
     private String name;
     private String description;
     private TreatmentRecommendation recommendation;
-}
+
+ }
--- backend/src/main/java/com/github/kormosbenedek/RecogniX/service/AutoTreatmentService.java
+++ backend/src/main/java/com/github/kormosbenedek/RecogniX/service/AutoTreatmentService.java
@@ -4,16 +4,35 @@ import com.github.kormosbenedek.RecogniX.dto.TreatmentDto;
 import com.github.kormosbenedek.RecogniX.entity.AutoTreatment;
 import com.github.kormosbenedek.RecogniX.entity.CompletedTreatment;
 import com.github.kormosbenedek.RecogniX.entity.Treatment;
+import com.github.kormosbenedek.RecogniX.enums.TreatmentRecommendation;
+import com.github.kormosbenedek.RecogniX.repositories.TreatmentCrudRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class AutoTreatmentService {
+    private final TreatmentCrudRepository repository;
 
     public List<TreatmentDto> getAutotreatments(Long requestTreatmentId){
         //lets do the filtering here
-        //TODO return everythin
+
+        //first, return everythin as it is
+        List<Treatment> treatments = (List<Treatment>) repository.findAll();
+        List<TreatmentDto> treatmentDtos = new ArrayList<>();
+        treatments.forEach(treatment -> {
+            TreatmentDto treatmentDto = new TreatmentDto(
+                    treatment.getId(),
+                    treatment.getName(),
+                    treatment.getDescription(),
+                    TreatmentRecommendation.NEUTRAL);
+            treatmentDtos.add(treatmentDto);
+        });
+
+        return treatmentDtos;
     }
 
 
