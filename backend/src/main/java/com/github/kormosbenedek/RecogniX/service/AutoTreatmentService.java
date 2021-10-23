cb80e2003cb4ab8582723f5f05432de2ba88228a
klucsik <krisztian.klucsik@pontsystems.eu>
2021-10-23 23:56:56 +0200

    autotreatment part1

--- backend/src/main/java/com/github/kormosbenedek/RecogniX/service/AutoTreatmentService.java
+++ backend/src/main/java/com/github/kormosbenedek/RecogniX/service/AutoTreatmentService.java
@@ -1,29 +1,43 @@
 package com.github.kormosbenedek.RecogniX.service;
 
 import com.github.kormosbenedek.RecogniX.dto.TreatmentDto;
-import com.github.kormosbenedek.RecogniX.entity.AutoTreatment;
-import com.github.kormosbenedek.RecogniX.entity.CompletedTreatment;
-import com.github.kormosbenedek.RecogniX.entity.Treatment;
+import com.github.kormosbenedek.RecogniX.entity.*;
 import com.github.kormosbenedek.RecogniX.enums.TreatmentRecommendation;
+import com.github.kormosbenedek.RecogniX.repositories.AutoTreatmentCrudRepository;
+import com.github.kormosbenedek.RecogniX.repositories.RequestTreatmentJpaRepository;
 import com.github.kormosbenedek.RecogniX.repositories.TreatmentCrudRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class AutoTreatmentService {
     private final TreatmentCrudRepository repository;
+    private final AutoTreatmentCrudRepository autoTreatmentRepository;
+    private final RequestTreatmentJpaRepository requestTreatmentRepository;
 
     public List<TreatmentDto> getAutotreatments(Long requestTreatmentId){
+        RequestTreatment requestTreatment = requestTreatmentRepository.getById(requestTreatmentId);
+        List<Symptom> symptomList = requestTreatment.getSymptomWithComments().stream().map(SymptomWithComment::getSymptom).collect(Collectors.toList());
         //lets do the filtering here
+        List<AutoTreatment> autoTreatments = (List<AutoTreatment>) autoTreatmentRepository.findAll();
+        autoTreatments.forEach(autoTreatment -> {
+
+        });
 
         //first, return everythin as it is
         List<Treatment> treatments = (List<Treatment>) repository.findAll();
+
+        return mapTreatmentToDto(treatments);
+    }
+
+    private List<TreatmentDto> mapTreatmentToDto(List<Treatment> treatmentList){
         List<TreatmentDto> treatmentDtos = new ArrayList<>();
-        treatments.forEach(treatment -> {
+        treatmentList.forEach(treatment -> {
             TreatmentDto treatmentDto = new TreatmentDto(
                     treatment.getId(),
                     treatment.getName(),
@@ -31,10 +45,8 @@ public class AutoTreatmentService {
                     TreatmentRecommendation.NEUTRAL);
             treatmentDtos.add(treatmentDto);
         });
-
         return treatmentDtos;
     }
-
 
     public void generateDraftAutoTreatment(CompletedTreatment completedTreatment){
         //just map and save the thingies
