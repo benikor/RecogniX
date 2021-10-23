61d16faf91e125f155a1eb765e9636e040684723
Kormos-git <kornorbi65@gmail.com>
2021-10-23 16:11:47 +0200

    all dummy data, allDataLoader fix for symptomWithComment

--- backend/src/main/java/com/github/kormosbenedek/RecogniX/dataloader/AllDataLoader.java
+++ backend/src/main/java/com/github/kormosbenedek/RecogniX/dataloader/AllDataLoader.java
@@ -1,8 +1,6 @@
 package com.github.kormosbenedek.RecogniX.dataloader;
 
-import com.github.kormosbenedek.RecogniX.entity.Patient;
-import com.github.kormosbenedek.RecogniX.entity.Symptom;
-import com.github.kormosbenedek.RecogniX.entity.Treatment;
+import com.github.kormosbenedek.RecogniX.entity.*;
 import com.github.kormosbenedek.RecogniX.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -19,6 +17,8 @@ public class AllDataLoader implements CommandLineRunner {
     @Autowired
     SymptomCrudRepository symptomCrudRepository;
     @Autowired
+    SymptomWithCommentCrudRepository symptomWithCommentCrudRepository;
+    @Autowired
     TreatmentCrudRepository treatmentCrudRepository;
     @Autowired
     RequestTreatmentCrudRepository requestTreatmentCrudRepository;
@@ -31,30 +31,77 @@ public class AllDataLoader implements CommandLineRunner {
     public void run(String... args) {
         loadPatientData();
         loadSymptomData();
+        loadSymptomWithCommentData();
         loadTreatmentData();
+        loadRequestTreatmentData();
     }
 
-    private void loadPatientData(){
+    private void loadPatientData() {
         List<Patient> patients = new ArrayList<>();
-        patients.add(new Patient(1L,"Teszt Elek",18));
-        patients.add(new Patient(2L,"Dummy Jhon",55));
+        patients.add(new Patient(1L, "Teszt Elek", 18));
+        patients.add(new Patient(2L, "Dummy Jhon", 55));
         patientCrudRepository.saveAll(patients);
     }
-    private void loadSymptomData(){
+
+    private void loadSymptomData() {
         List<Symptom> symptoms = new ArrayList<>();
-        symptoms.add(new Symptom(1L,"Headache",loremIpsum));
-        symptoms.add(new Symptom(2L,"Fatigue",loremIpsum));
-        symptoms.add(new Symptom(3L,"Skin redness",loremIpsum));
-        symptoms.add(new Symptom(4L,"Pain",loremIpsum));
-        symptoms.add(new Symptom(5L,"Loss of appetite",loremIpsum));
-        symptoms.add(new Symptom(6L,"Urinary retention",loremIpsum));
+        symptoms.add(new Symptom(1L, "Headache", loremIpsum,2));
+        symptoms.add(new Symptom(2L, "Fatigue", loremIpsum,1));
+        symptoms.add(new Symptom(3L, "Skin redness", loremIpsum,2));
+        symptoms.add(new Symptom(4L, "Pain", loremIpsum,3));
+        symptoms.add(new Symptom(5L, "Loss of appetite", loremIpsum,3));
+        symptoms.add(new Symptom(6L, "Urinary retention", loremIpsum,5));
         symptomCrudRepository.saveAll(symptoms);
     }
-    private void loadTreatmentData(){
+    private void loadSymptomWithCommentData() {
+        List<SymptomWithComment> symptomWithComments = new ArrayList<>();
+        symptomWithComments.add(new SymptomWithComment(1L,symptomCrudRepository.findById(1L).get(),"comment for headache"));
+        symptomWithComments.add(new SymptomWithComment(2L,symptomCrudRepository.findById(2L).get(),"comment for fatigue"));
+        symptomWithComments.add(new SymptomWithComment(3L,symptomCrudRepository.findById(3L).get(),"comment for skin redness"));
+        symptomWithComments.add(new SymptomWithComment(4L,symptomCrudRepository.findById(4L).get(),"comment for pain"));
+        symptomWithComments.add(new SymptomWithComment(5L,symptomCrudRepository.findById(5L).get(),"comment for loss of appetite"));
+        symptomWithComments.add(new SymptomWithComment(6L,symptomCrudRepository.findById(6L).get(),"comment for urinary retention"));
+        symptomWithCommentCrudRepository.saveAll(symptomWithComments);
+    }
+
+    private void loadTreatmentData() {
         List<Treatment> treatments = new ArrayList<>();
-        treatments.add(new Treatment(1L,"Take pills","1 x AlgoFlex 400mg, twice daily"));
-        treatments.add(new Treatment(2L,"Rest","Take more rest until it ease"));
-        treatments.add(new Treatment(3L,"Call Ambulance","Call the ambulance immediately!"));
+        treatments.add(new Treatment(1L, "Take pills", "1 x AlgoFlex 400mg, twice daily"));
+        treatments.add(new Treatment(2L, "Rest", "Take more rest until it ease"));
+        treatments.add(new Treatment(3L, "Call Ambulance", "Call the ambulance immediately!"));
         treatmentCrudRepository.saveAll(treatments);
     }
+
+    private void loadRequestTreatmentData() {
+
+        List<List<SymptomWithComment>> symptomLists = new ArrayList<>();
+        symptomLists.add(new ArrayList<>());
+        symptomLists.get(0).add(symptomWithCommentCrudRepository.findById(1L).get());
+        symptomLists.get(0).add(symptomWithCommentCrudRepository.findById(3L).get());
+        symptomLists.add(new ArrayList<>());
+        symptomLists.get(1).add(symptomWithCommentCrudRepository.findById(4L).get());
+        symptomLists.get(1).add(symptomWithCommentCrudRepository.findById(5L).get());
+        symptomLists.add(new ArrayList<>());
+        symptomLists.get(2).add(symptomWithCommentCrudRepository.findById(6L).get());
+
+        List<RequestTreatment> requestTreatments = new ArrayList<>();
+
+        requestTreatments.add(new RequestTreatment(
+                1L,
+                patientCrudRepository.findById(1L).get(),
+                symptomLists.get(0)));
+
+        requestTreatments.add(new RequestTreatment(
+                2L,
+                patientCrudRepository.findById(1L).get(),
+                symptomLists.get(1)));
+
+        requestTreatments.add(new RequestTreatment(
+                2L,
+                patientCrudRepository.findById(1L).get(),
+                symptomLists.get(2)));
+
+        requestTreatmentCrudRepository.saveAll(requestTreatments);
+    }
+
 }
--- backend/src/main/java/com/github/kormosbenedek/RecogniX/entity/RequestTreatment.java
+++ backend/src/main/java/com/github/kormosbenedek/RecogniX/entity/RequestTreatment.java
@@ -1,5 +1,6 @@
 package com.github.kormosbenedek.RecogniX.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.util.List;
 @Entity
 @NoArgsConstructor
 @Data
+@AllArgsConstructor
 public class RequestTreatment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
